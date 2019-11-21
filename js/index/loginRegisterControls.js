c389ac597f97eca179a2c14232e51efbc379f455
dingyx99 <dingyuxiao99@nwafu.edu.cn>
2019-11-21 22:08:48 +0800

    Merge Code

--- js/index/loginRegisterControls.js
+++ js/index/loginRegisterControls.js
@@ -29,10 +29,9 @@ function onInputValidate(type) {
     var valGroup = ["email", "username", "telephone", "password", "password2"];
     var idGroup = ["#regEmail", "#regUsername", "#phoneNum", "#regPass", "#regPass2"];
     var iconGroup = ["mail-icon", "username-icon", "phone-icon", "password-icon", "password2-icon"];
-    if(type != 4){
+    if (type != 4) {
         valStatus = Validation(valGroup[type], $(idGroup[type]).val());
-    }
-    else {
+    } else {
         valStatus = Validation($(idGroup[3]).val(), $(idGroup[4]).val());
     }
     if (valStatus) {
@@ -57,7 +56,7 @@ function finalVal() {
 
 function Validation(type, string) {
     var uPattern = /^[a-zA-Z0-9_-]{4,16}$/; //Username
-    var ePattern = /^([A-Za-z0-9_\-\.\u4e00-\u9fa5])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,8})$/; //Email
+    var ePattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/; //Email
     var pPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\s\S]{8,16}$/; //Password
     var tPattern = /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$/; //Telephone
 
@@ -94,4 +93,75 @@ function toggleButton(id, request) {
         document.getElementById(id).disabled = false;
         document.getElementById(id).className = "btn btn-primary";
     } //Enable Button
+}
+
+function getXMLHttpRequest() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xmlhttp;
+}
+
+function register() {
+    var regEmail = $("#regEmail").val();
+    var regUsername = $("#regUsername").val();
+    var phoneNum = $("#phoneNum").val();
+    var regPass = $("#regPass").val();
+
+    //Ajax
+    var req = getXMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                if (req.responseText == "email") {
+                    document.getElementById("registerAlert").innerHTML = "邮箱已注册";
+                    document.getElementById("registerAlert").className = "alert alert-danger";
+                    document.getElementById("mail-icon").className = "input-group-text bg-danger text-white"
+                    toggleButton("regButton", 0);
+                } else if (req.responseText == "username") {
+                    document.getElementById("registerAlert").innerHTML = "用户名已存在";
+                    document.getElementById("registerAlert").className = "alert alert-danger";
+                    document.getElementById("username-icon").className = "input-group-text bg-danger text-white"
+                    toggleButton("regButton", 0);
+                } else if (req.responseText == "email_username") {
+                    document.getElementById("registerAlert").innerHTML = "邮箱已注册;   用户名已存在";
+                    document.getElementById("registerAlert").className = "alert alert-danger";
+                    document.getElementById("mail-icon").className = "input-group-text bg-danger text-white"
+                    document.getElementById("username-icon").className = "input-group-text bg-danger text-white"
+                    toggleButton("regButton", 0);
+                } else {}
+            }
+        }
+    }
+    req.open("get", "../RegisterAction?regEmail=" + regEmail + "&regUsername=" + regUsername + "&phoneNum=" + phoneNum + "&regPass=" + regPass);
+    req.send(null);
+}
+
+
+function login() {
+    var loginEmail = $("#loginEmail").val();
+    var loginPass = $("#loginPassword").val();
+
+    $("#LoginPopUp").modal('hide');
+
+    //Ajax
+    var req = getXMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                if (req.responseText == "username") {
+                    document.getElementById("loginMsg").innerHTML = "用户不存在";
+                } else if (req.responseText == "password") {
+                    document.getElementById("loginMsg").innerHTML = "密码错误";
+                } else {
+                    document.getElementById("loginMsg").innerHTML = "你已成功登录";
+                }
+            }
+        }
+    }
+    req.open("get", "../LoginAction?loginEmail=" + loginEmail + "&loginPass=" + loginPass);
+    req.send(null);
 }
